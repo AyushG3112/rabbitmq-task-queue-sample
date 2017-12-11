761fb07425dcf0e37eadc3f8f27740ebc8db6df7
Ayush Gupta <AyushG3112@gmail.com>
2017-12-11 19:16:12 +0530

    basic form

--- client/angular/src/app/app.component.ts
+++ client/angular/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  messageForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.createForm()
+  }
+
+  createForm() {
+    this.messageForm = this.fb.group({
+      message: ['', Validators.required], // <--- the FormControl called "name"
+      delay: [, [Validators.required, Validators.min(1)]], // <--- the FormControl called "name"
+    });
+  }
+
+  onSubmit(event: any) {
+    console.log(event)
+  }
 }
